f55442fa2dd26f7d2adb76d6e9a9179463582f27
general <463734522@qq.com>
2013-09-26 14:22:33 +0800

    修改了一些

--- src/main/java/cn/newgxu/bbs/web/action/accounts/CheckAccoutAction.java
+++ src/main/java/cn/newgxu/bbs/web/action/accounts/CheckAccoutAction.java
@@ -1,5 +1,8 @@
 package cn.newgxu.bbs.web.action.accounts;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import cn.newgxu.bbs.common.util.JsonUtil;
 import cn.newgxu.bbs.common.util.Util;
 import cn.newgxu.bbs.domain.user.User;
@@ -13,16 +16,23 @@ public class CheckAccoutAction extends ActionSupport {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
-	private String userName;
+	private static final Log log = LogFactory.getLog(CheckAccoutAction.class);
+	private String username;
 	private String passWord;
-	public String getUserName() {
-		return userName;
+	
+	
+	public static Log getLog() {
+		return log;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
+	
+	public String getUsername() {
+		return username;
 	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
 	public String getPassWord() {
 		return passWord;
 	}
@@ -33,10 +43,10 @@ public class CheckAccoutAction extends ActionSupport {
 	 * 用来作为检验账户和密码是否正确的接口 正确返回true 错误返回false
 	 */
 	public String execute() throws ObjectNotFoundException{
-		System.out.println(userName);
+		
 		String pass = Util.hash(passWord);
-		System.out.println("加密后："+pass);
-		User user = User.getByUsername(userName);
+		log.info("加密后："+pass);
+		User user = User.getByUsername(username);
 		if(user.getPassword().equals(pass)){
 			JsonUtil.sendJsonStr("{\"result\":\"true\"}");
 		}else{
